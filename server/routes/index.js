1501fc7d4cef51a5daf5c1948d09d68dcf3ba68f
carlos andres fajardo pedraza <carlosafp84@hotmail.com>
2020-08-22 12:57:49 -0500

    ajuste producto delete

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,8 +2,7 @@ const express = require('express');
 const app = express();
 // Importamos el usuario
 app.use(require('./usuario'));
-app.use(require('./categoria'));
 app.use(require('./login'));
-app.use(require('./producto'));
+app.use(require('./categoria'));
 
 module.exports = app;
